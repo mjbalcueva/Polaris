f7c4fdbfc835ec074f0dac5cdac57bcb09c70d61
Mark John Balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-10-03 01:03:32 +0800

    feat: add app layout

--- Polaris/Root.Designer.cs
+++ Polaris/Root.Designer.cs
@@ -28,15 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SidePanel = new System.Windows.Forms.Panel();
+            this.TitlePanel = new System.Windows.Forms.Panel();
+            this.EmbedPanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // SidePanel
+            // 
+            this.SidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(33)))));
+            this.SidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SidePanel.Location = new System.Drawing.Point(0, 0);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(250, 553);
+            this.SidePanel.TabIndex = 0;
+            // 
+            // TitlePanel
+            // 
+            this.TitlePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(21)))), ((int)(((byte)(24)))));
+            this.TitlePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TitlePanel.Location = new System.Drawing.Point(250, 0);
+            this.TitlePanel.Name = "TitlePanel";
+            this.TitlePanel.Size = new System.Drawing.Size(632, 70);
+            this.TitlePanel.TabIndex = 1;
+            // 
+            // EmbedPanel
+            // 
+            this.EmbedPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(21)))), ((int)(((byte)(24)))));
+            this.EmbedPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EmbedPanel.Location = new System.Drawing.Point(250, 70);
+            this.EmbedPanel.Name = "EmbedPanel";
+            this.EmbedPanel.Size = new System.Drawing.Size(632, 483);
+            this.EmbedPanel.TabIndex = 2;
             // 
             // Root
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 453);
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.EmbedPanel);
+            this.Controls.Add(this.TitlePanel);
+            this.Controls.Add(this.SidePanel);
             this.Font = new System.Drawing.Font("Nobile", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MinimumSize = new System.Drawing.Size(850, 500);
             this.Name = "Root";
             this.ShowIcon = false;
@@ -46,6 +79,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel SidePanel;
+        private System.Windows.Forms.Panel TitlePanel;
+        private System.Windows.Forms.Panel EmbedPanel;
     }
 }
 
