42216972fb2535c939c42f3caf76ffb750a058d4
Mark John Balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-10-02 22:40:05 +0800

    refactor: change form details to fit project

--- Polaris/Root.Designer.cs
+++ Polaris/Root.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Polaris
 {
-    partial class Form1
+    partial class Root
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Root
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Root";
+            this.ShowIcon = false;
+            this.Text = "Polaris";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Polaris/Root.cs
+++ Polaris/Root.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Polaris
 {
-    public partial class Form1 : Form
+    public partial class Root : Form
     {
-        public Form1()
+        public Root()
         {
             InitializeComponent();
         }
